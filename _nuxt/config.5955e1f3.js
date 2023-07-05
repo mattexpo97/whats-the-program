8fa15062ef70bc8bc00403536779ce77a1865875
Mattis Exposito <mattisexposito@MBP-de-Mattis.lan>
2023-07-05 14:30:00 +0200

    Updates

---
+++ _nuxt/config.5955e1f3.js
@@ -0,0 +1,1 @@
+import{b9 as n,p as o,ba as i}from"./entry.f2d2cc7e.js";const a={},f=n(a);function s(){const p=o();return p._appConfig||(p._appConfig=i(f)),p._appConfig}export{s as u};
--- _nuxt/config.ac2ff23d.js
+++
@@ -1,1 +0,0 @@
-import{b9 as n,p as o,ba as i}from"./entry.999e175f.js";const a={},f=n(a);function s(){const p=o();return p._appConfig||(p._appConfig=i(f)),p._appConfig}export{s as u};
